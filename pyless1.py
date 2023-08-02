dff47b7dcd553fd81631b9aeff03f58af32f1e69
VIRTUOSO <kostyakot817@gmail.com>
2023-08-02 19:20:29 +0500

    comm1

--- pyless1.py
+++ pyless1.py
@@ -1,2 +1,25 @@
-a = 1+2
-print(a)
+""" x = 5
+i = 1
+res = 1
+while i<=x:
+    res = res * i
+    i+=1
+ 
+print(res)
+ """
+
+""" a = 5
+first = 1
+sec = 0
+i = 2 
+while first < a:
+    tmp = first
+    first = sec + first
+    sec = tmp
+
+    i+=1
+if first == a:
+    print(i)
+else:
+    print(-1) """
+
